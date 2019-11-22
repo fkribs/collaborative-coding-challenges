685436405f4ab538b567efaddb6835015ecd04be
Forrest Kribs <FKribs@harriscomputer.com>
2019-11-22 14:08:53 -0700

    Added Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,33 @@
+// A palindrome is a word that is identical forward and backwards.
+
+// mom
+// racecar
+// kayak
+// Given a word, create a function that checks whether it is a palindrome.
+
+// Examples
+// checkPalindrome("mom") ➞ true
+
+// checkPalindrome("scary") ➞ false
+
+// checkPalindrome("reviver") ➞ true
+
+// checkPalindrome("stressed") ➞ false
+// Notes
+// All test input is lower cased.
+
+class Program {
+    public static void main(String[] args) {
+        String value = "mom";
+        System.out.println(KevinSolution(value));
+        System.out.println(ForrestSolution(value));
+    }
+
+    public static boolean KevinSolution(String str){
+        return false;
+    }
+
+    public static boolean ForrestSolution(String str){
+        return false;
+    }
+}
