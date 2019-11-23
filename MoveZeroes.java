4a1a03b825ab79a15c8f2ba532585fd2ab28bbcf
kevokvr <kvalenzu@msudenver.edu>
2019-11-22 21:28:28 -0700

    kevins solution

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -26,7 +26,16 @@ class Program {
     }
 
     public static void KevinSolution(int[] nums){
-
+        int left = 0;
+        int temp;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                temp = nums[left];
+                nums[left] = nums[i];
+                nums[i] = temp;
+                left++;
+            }
+        }
     }
 
 
