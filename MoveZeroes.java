12f5322bbe05886f44145e6ca63fff2410f46e80
fkribs <ffkribs@gmail.com>
2019-11-22 22:18:48 -0700

    reformatting

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -16,13 +16,12 @@ class Program {
         int[] array1 = new int[]{0, 1, 0, 3, 12};
         int[] array2 = new int[]{0, 1, 0, 3, 12};
     
-        ForrestSolution(array1);
+        ForrestSolution2(array1);
         System.out.println(Arrays.toString(array1));
         KevinSolution(array2);
         System.out.println(Arrays.toString(array2));
 
     }
-
     public static void ForrestSolution(int[] nums) {
         // int numZeroes = 0;
         int length = nums.length;
@@ -36,17 +35,6 @@ class Program {
             }
         }
     }
-
-        // int[] array1 = new int[] { 0, 1, 1, 0, 3, 12, 15, 15};
-        // 0, 1, 1, 0, 3, 12, 15, 15
-        // 1, 0, 1, 0, 3, 12, 15, 15
-        // 1, 1, 0, 0, 3, 12, 15, 15
-        // 1, 1, 3, 0, 0, 12, 15, 15
-        // 1, 1, 3, 12, 0, 0, 15, 15
-        // 1, 1, 3, 12, 15, 0, 0, 15
-        // 1, 1, 3, 12, 15, 0, 0, 15
-        // 1, 1, 3, 12, 15, 15, 0, 0
-
     public static void ForrestSolution2(int[] nums) {
         int length = nums.length;
         for (int i = 0; i < length; i++) {
@@ -54,23 +42,18 @@ class Program {
                 continue;
             int numZeroes = 1;
             for (int i2 = i; i2 < length - numZeroes; i2++) {
-                // System.out.println("index:\t" + i2);
                 int next = nums[i2 + numZeroes];
-                // System.out.println("next:\t" + next);
                 if (next == 0) {
                     numZeroes++;
                     i2--;
                     continue;
                 }
-                // System.out.println("numZeroes:\t" + numZeroes + "\n");
                 int temp = nums[i2];
                 nums[i2] = nums[i2 + numZeroes];
                 nums[i2 + numZeroes] = temp;
-                // System.out.println(Arrays.toString(nums));
             }
         }
     }
-
     public static void KevinSolution(int[] nums){
         int left = 0;
         int temp;
@@ -83,6 +66,4 @@ class Program {
             }
         }
     }
-
-
 }
