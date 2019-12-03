a2a37b6154ba56db61b0d639883fcf66b2585fa1
kevokvr <kvalenzu@msudenver.edu>
2019-12-02 21:34:47 -0700

    water container solution

--- WaterContainer.java
+++ WaterContainer.java
@@ -32,8 +32,21 @@ class WaterContainer{
         return max;
     }
 
+    //two pointer approach
     public static int maxAreaKevin(int[] height){
-        //processing
-        return -1;
+        int max = Integer.MIN_VALUE;
+        int left = 0;
+        int right = height.length - 1;
+        
+        while(left <= right){
+            if(height[left] < height[right]){
+                max = Math.max(max, height[left] * (right - left));
+                left++;
+            }else{
+                max = Math.max(max, height[right] * (right - left));
+                right--;
+            }
+        }
+        return max;
     }
 }
