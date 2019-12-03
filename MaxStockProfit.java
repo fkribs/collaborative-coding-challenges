e1630f11b345872254f64802061f47dd2793b7f0
kevokvr <kvalenzu@msudenver.edu>
2019-12-02 21:55:22 -0700

    description

--- MaxStockProfit.java
+++ MaxStockProfit.java
@@ -1,4 +1,5 @@
-/*Best Time to Buy and Sell Stock
+/*
+Best Time to Buy and Sell Stock
 Say you have an array for which the ith element is the price of a given stock on day i.
 If you were only permitted to complete at most one transaction 
 (i.e., buy one and sell one share of the stock), design an algorithm to find the MAXIMUM profit.
@@ -19,7 +20,7 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 
 class ProgramTemplate {
     public static void main(String[] args) {
-        
+
     }
 
     public int maxProfitForrest(int[] prices) {
