5c9b737fe0dbdc82878e1889e0ee25d775fba9e6
Forrest Kribs <FKribs@harriscomputer.com>
2019-12-13 10:27:58 -0700

    completed pandigital test harness

--- PandigitalNumbers.java
+++ PandigitalNumbers.java
@@ -5,58 +5,68 @@
 // // 7 is missing.
 
 // isPandigital(112233445566778899) ➞ false
-import java.util.Arrays;
-import java.util.Random;
-import java.util.function.Consumer;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+class Result{
+    public long ElapsedTime;
+    public boolean Succeeded;
+    public Result(boolean succeeded, long elapsedTime){
+        this.Succeeded = succeeded;
+        this.ElapsedTime = elapsedTime;
+    }
+}
 
 class PandigitalNumbers {
+    public static void main(String[] args) {
+        Test(12345678, false);
+        Test(123456789, true);
+    }
 
     private interface IPandigital {
         boolean isPandigital(int input);
     }
 
-    public static void main(String[] args) {
-        Test(12345678);
-    }
-
-    private static void Test(int input) {
-
-        String label = "Forrest";
-        IPandigital forrestSolution = (i) -> {
-            return ForrestSolution(i);
-        };
-
-        long elapsed = Time(forrestSolution, input);
-        System.out.println(label + ": " + elapsed + " milliseconds");
-
-        IPandigital kevinSolution = (i) -> {
-            return KevinSolution(i);
-        };
-
-        label = "Kevin";
-        elapsed = Time(kevinSolution, input);
-        System.out.println(label + ": " + elapsed + " milliseconds");
-    }
-
-    private static long Time(IPandigital method, int input) {
-        long startTime = System.currentTimeMillis();
-        method.isPandigital(input);
-        long stopTime = System.currentTimeMillis();
-        long elapsedTime = stopTime - startTime;
-        return elapsedTime;
-    }
-
-    private static boolean UnitTest() {
-        return false;
-    }
 
     private static boolean ForrestSolution(int input) {
-        return true;
+        return false;
     }
 
     private static boolean KevinSolution(int input) {
         return true;
+    }
+
+    private static void Test(int input, boolean expectedResult) {
+        System.out.println("\nTesting input '" + input +  "' with expectedResult '" + expectedResult + "'");
+        String label;
+        IPandigital method;
+        Result result;
+        // Forrest
+        label = "Forrest";
+        method = (i) -> {
+            return ForrestSolution(i);
+        };
+        result = TimeAndTest(method, input, expectedResult);
+        PrintResult(label, result);
+
+        // Kevin
+        label = "Kevin";
+        method = (i) -> {
+            return KevinSolution(i);
+        };
+        result = TimeAndTest(method, input, expectedResult);
+        PrintResult(label, result);
+    }
+
+    private static void PrintResult(String label, Result result){
+        String successStatus = result.Succeeded ? "Succeeded" : "Failed";
+        String message = "\t" + label + ": " + successStatus + " in " + result.ElapsedTime + " milliseconds";
+        System.out.println(message);
+    }
+
+    private static Result TimeAndTest(IPandigital method, int input, boolean expectedResult) {
+        long startTime = System.currentTimeMillis();
+        boolean result = method.isPandigital(input);
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        return new Result(result == expectedResult, elapsedTime);
     }
 }
